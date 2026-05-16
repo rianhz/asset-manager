8c1dec7b6843d06d7cff6f23e85efe371c39fe3d
Rian Hidayat <rian.hidayat@softwareseni.com>
2026-05-16 12:38:15 +0700

    initialize api

---
+++ api/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json({
+    message: "API running 🚀",
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
